79eaeea3c27f44f63d94947e1505488c51369451
256791 <krosnymichal@gmail.com>
2025-01-19 12:10:01 +0100

    Fix linter issue

--- main.go
+++ main.go
@@ -36,6 +36,8 @@ func main() {
 			"result": add(a, b),
 		})
 	})
-
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if err := r.Run(); err != nil {
+		panic(err)
+	}
 }
